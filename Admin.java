f229967d5bed7b01682f9a4d93d40eebdedba04a
18xxperson <46502267+18xxperson@users.noreply.github.com>
2022-03-01 21:48:01 -0800

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -1,7 +1,11 @@
 package project0;
 
 public class Admin extends Employee{
-    public static void implement_deposit(Customer c,int amount)
+    public static void implement_deposit(Customer c,int amount, int id)
+    {
+    	
+    }
+    public static void implement_withdraw(Customer c, int amount, int id)
     {
     	
     }
